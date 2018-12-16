46244c139cb5d2b3d11c274bc513fcf5ff33347d
Peter <peter.ly@stonybrook.edu>
2018-12-16 06:04:36 -0500

    Menu rework, added small help menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,7 +7,9 @@ public class MenuController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		GameObject help = GameObject.Find("HelpCanvas");
+		help.transform.GetChild (0).gameObject.SetActive(false);
+		//help.GetComponent ("Panel").enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -17,6 +19,17 @@ public class MenuController : MonoBehaviour {
 
 	public void startTheGame(){
 		SceneManager.LoadSceneAsync ("Level1");
+	}
+	public void loadLevel(string levelname){
+		SceneManager.LoadSceneAsync (levelname);
+	}
+	public void displayHelpMenu(){
+		GameObject help = GameObject.Find("HelpCanvas");
+		if (help.transform.GetChild (0).gameObject.activeSelf == false) {
+			help.transform.GetChild (0).gameObject.SetActive (true);
+		} else {
+			help.transform.GetChild (0).gameObject.SetActive (false);
+		}
 	}
 	public void quit(){
 		Application.Quit ();
